85961fe3376f5d815b9be5f8cbf2870e893fba01
henriquexpaulo <phvieira3@gmail.com>
2023-05-19 00:19:26 -0300

    Conclusão da Calculadora

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+function criaCalculador() {
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert("Conta invalida");
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta invalida')
+                return;
+            }
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        cliqueBotoes() {
+
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+                this.display.focus();
+
+            });
+        },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+    };
+}
+
+const calculadora = criaCalculador();
+calculadora.inicia()
